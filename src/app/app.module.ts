c9ad06cef19f2d80159e9b4c3f607a0c3e0a0f7b
Ewelina Dziewiela <ewelina.dziewiela@ravelo.pl>
2017-04-03 14:26:55 +0200

    newcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { Form } from "./form-component/form.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Form
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
-  ],
-  providers: [],
+    HttpModule,
+    RouterModule.forRoot([
+        { path: 'login', component: Form }
+  ])],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
